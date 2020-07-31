1fc0a4820190757a13263f2f3ef98bf1270d0704
Дмитрий Пирязев <ciaemon@LIVE.RU>
2020-07-31 22:07:06 +0700

    Stage 1 completed.
    Printing the field

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,22 @@
 package minesweeper
 
+import java.util.*
+import kotlin.random.Random
+
+fun create(): Char {
+    val random = Random.Default;
+    return if (random.nextBoolean()) '.' else 'X'
+}
 fun main() {
+
+    val sc = Scanner(System.`in`)
+    val sizeX = 5 //sc.nextInt()
+    val sizeY = 5 //sc.nextInt()
+    for (i in 1..sizeX) {
+        for (j in 1..sizeY) {
+            print(create())
+        }
+        println()
+    }
 
 }
